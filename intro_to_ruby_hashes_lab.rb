cb172cf345b7bb399c09ee4af131ff4e6069631e
charming-input-2976 <mbingramm@gmail.com>
2020-11-25 19:17:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,10 @@
 def base_hash
-  { :railroads => {} }
+  { railroads: {} }
 
 end
 
 def monopoly_with_second_tier
-  :railroads = {
+  {railroads: {
     :pieces => "4"
     :rent_in_dollars => {}
     :names => {}
